deb37d69d2b7400f4d52445e34d5e9a8b6718a8d
kfirkoren <kfirmail@gmail.com>
2024-08-02 16:23:00 +0300

    trip

---
+++ MateClientSide/src/components/DropDown/DropDown.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native'
+import { FontAwesome } from '@expo/vector-icons'
+import { VerticalScale } from '../../utils'
+
+const data = [
+  { label: 'להלהלהל', value: '1' },
+  { label: 'Item 2', value: '2' },
+  { label: 'Item 3', value: '3' },
+  { label: 'Item 4', value: '4' },
+  { label: 'Item 5', value: '5' },
+  { label: 'Item 6', value: '6' },
+  { label: 'Item 7', value: '7' },
+  { label: 'Item 8', value: '8' },
+]
+
+const DropDown = () => {
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false)
+
+  const handleDropdownPress = () => {
+    setIsDropdownVisible(!isDropdownVisible)
+  }
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.dropdown} onPress={handleDropdownPress}>
+        <Text style={styles.selectedTextStyle}>יעדים</Text>
+        <FontAwesome
+          name={isDropdownVisible ? 'angle-up' : 'angle-down'}
+          size={20}
+          color='#000'
+        />
+      </TouchableOpacity>
+      {isDropdownVisible && (
+        <ScrollView style={styles.dropdownContent} nestedScrollEnabled={true}>
+          {data.map((item) => (
+            <View key={item.value} style={styles.dropdownItem}>
+              <Text style={styles.dropdownItemText}>{item.label}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      )}
+    </View>
+  )
+}
+
+export default DropDown
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: VerticalScale(5),
+    width: '80%',
+  },
+  dropdown: {
+    height: 50,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    borderColor: '#ccc',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row-reverse', // כיוון מימין לשמאל
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+    textAlign: 'right', // יישור לימין
+    writingDirection: 'rtl', // כיוון כתיבה מימין לשמאל
+  },
+  dropdownContent: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    marginTop: 8,
+    maxHeight: 150, // הגדרת גובה מקסימלי לרשימה הנגללת
+  },
+  dropdownItem: {
+    padding: 10,
+    flexDirection: 'row-reverse', // כיוון מימין לשמאל עבור הפריטים
+    justifyContent: 'flex-start', // יישור הפריטים לימין
+  },
+  dropdownItemText: {
+    fontSize: 16,
+    textAlign: 'right', // יישור לימין
+    writingDirection: 'rtl', // כיוון כתיבה מימין לשמאל
+  },
+})
--- MateClientSide/src/navigation/Navigation.jsx
+++ MateClientSide/src/navigation/Navigation.jsx
@@ -1,37 +1,37 @@
-import * as React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import Register from "../pages/register";
-import Login from "../pages/login";
-import Splash from "../pages/splash";
-import Intro from "../pages/intro";
-import EditProfile from "../pages/edit_profile";
-import PlanTrip from "../pages/plan_trip";
-import ViewProfile from "../pages/view_profile";
-import Home from "../pages/home";
-import Tabs from "./tabs";
+import * as React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import Register from '../pages/register'
+import Login from '../pages/login'
+import Splash from '../pages/splash'
+import Intro from '../pages/intro'
+import EditProfile from '../pages/edit_profile'
+import PlanTrip from '../pages/plan_trip'
+import ViewProfile from '../pages/view_profile'
+import Home from '../pages/home'
+import Tabs from './tabs'
+import Trip from '../pages/trip'
 const MainNavigation = () => {
-  const Stack = createStackNavigator();
+  const Stack = createStackNavigator()
 
   return (
     <Stack.Navigator
-      initialRouteName="Loading"
+      initialRouteName='Loading'
       screenOptions={{ header: () => null, headerShown: false }}
     >
-      <Stack.Screen name="Splash" component={Splash} />
-      <Stack.Screen name="Intro" component={Intro} />
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Register" component={Register} />
-      <Stack.Screen name="EditProfile" component={EditProfile} />
-      <Stack.Screen name="ViewProfile" component={ViewProfile} />
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="myTabs" component={Tabs} />
-
+      <Stack.Screen name='Trip' component={Trip} />
+      <Stack.Screen name='Splash' component={Splash} />
+      <Stack.Screen name='Intro' component={Intro} />
+      <Stack.Screen name='Login' component={Login} />
+      <Stack.Screen name='Register' component={Register} />
+      <Stack.Screen name='EditProfile' component={EditProfile} />
+      <Stack.Screen name='ViewProfile' component={ViewProfile} />
+      <Stack.Screen name='Home' component={Home} />
+      <Stack.Screen name='myTabs' component={Tabs} />
 
       {/* <Stack.Screen name="PlanTrip" component={PlanTrip} /> */}
       {/* <Stack.Screen name="MyTabs" component={MyTabs} /> */}
     </Stack.Navigator>
-  );
-};
+  )
+}
 
-export default MainNavigation;
-
+export default MainNavigation
