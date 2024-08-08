9dc01a56e9f56910ee76e4af3270f0a073cf6bfd
barleviatias <barlevi.atias@gmail.com>
2024-08-08 17:08:40 +0300

    add view trips to the profile

--- MateClientSide/src/pages/view_profile/index.jsx
+++ MateClientSide/src/pages/view_profile/index.jsx
@@ -1,42 +1,78 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
-import React, { useStat,useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
+import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native'
+import axios from 'axios'
 import Theme from '../../../assets/styles/theme'
 import { VerticalScale, windowHeight } from '../../utils'
 import BackArrow from '../../components/BackArrow/backArrow'
-import { TextInput, Button } from 'react-native-paper'
-import Input from '../../components/Input/input'
-import { AuthContext } from '../../../AuthContext' // Adjust the import path as needed
-import ButtonLower from '../../components/ButtonLower/buttonLower'
-import { Avatar } from 'react-native-paper'
+import { Button, Avatar } from 'react-native-paper'
 import TextView from '../../components/TextView/textView'
 import TagsView from '../../components/TagsView/tagsView'
 import { SingleCharToString } from '../../utils'
 import { useRoute } from '@react-navigation/native'
+import DropDown from '../../components/DropDown/DropDown'
+import { AuthContext } from '../../../AuthContext'
+import SingleTrip from '../../components/SingleTrip/singleTrip' // Assuming SingleTrip is another component
 
-export default function ViewProfile({navigation}) {
-  const route = useRoute();
-  // const navigation = useNavigation();
-  const { loggedInUser } = useContext(AuthContext);
-  const isOwnProfile = !route.params?.profile;
+export default function ViewProfile({ navigation }) {
+  const route = useRoute()
+  const { loggedInUser } = useContext(AuthContext)
+  const isOwnProfile = !route.params?.profile
+
+  const [tripsRenderData, setTripsRenderData] = useState([]);
+  const [tripsCurrentPage, setTripsCurrentPage] = useState(1);
+  const [tripsPageSize] = useState(10);
+  const [isLoadingTrips, setIsLoadingTrips] = useState(false);
+
+  useEffect(() => {
+    // Call getAllUserTrips when the component mounts
+    getAllUserTrips();
+    
+  }, []);
+
+  const getAllUserTrips = async () => {
+    try {
+      const response = await axios.post(
+        `https://us-central1-mateapiconnection.cloudfunctions.net/mateapi/getUserTrips`,
+        {
+          uid: loggedInUser.uid
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        }
+      )
+      setTripsRenderData(response.data);
+    } 
+    catch (error) {
+      console.error('Error', error.message)
+    }
+  }
 
   const handleEditProfile = () => {
-    navigation.navigate('EditProfile', { profile: loggedInUser });
-    console.log("edit progfile");
-    
-    // Navigate to the EditProfile screen
-    // You'll need to create this screen and set up the navigation
-  };
-  // Use route params if available, otherwise use context
-  const profile = route.params?.profile || loggedInUser;
+    navigation.navigate('EditProfile', { profile: loggedInUser })
+    console.log('edit profile')
+  }
+
+  const profile = route.params?.profile || loggedInUser
 
   if (!profile) {
     return (
       <View style={styles.screen}>
         <Text>No profile data available</Text>
       </View>
-    );
+    )
   }
-  
+
+  const pagination = (database, currentPage, pageSize) => {
+    const startIndex = (currentPage - 1) * pageSize
+    const endIndex = startIndex + pageSize
+    if (startIndex >= database.length) {
+      return []
+    }
+    return database.slice(startIndex, endIndex)
+  }
+
   return (
     <ScrollView
       contentContainerStyle={styles.screen}
@@ -44,16 +80,13 @@ export default function ViewProfile({navigation}) {
     >
       <BackArrow />
       <View style={styles.avatarContainer}>
-        <Avatar.Image
-          size={150}
-          source={{uri:profile.profileImage}}
-        />
+        <Avatar.Image size={150} source={{ uri: profile.profileImage }} />
         <Text style={[Theme.primaryTitle, styles.text]}>
-          {profile.fullname.split(' ')[0]},{profile.age}
+          {profile.fullname.split(' ')[0]}, {profile.age}
         </Text>
         {isOwnProfile && (
-          <Button 
-            mode="contained" 
+          <Button
+            mode='contained'
             onPress={handleEditProfile}
             style={styles.editButton}
           >
@@ -62,31 +95,69 @@ export default function ViewProfile({navigation}) {
         )}
       </View>
       <View style={styles.inputsContainer}>
-  
         <TextView
           title={'מין'}
           content={SingleCharToString(profile.gender)}
           iconName={
-            SingleCharToString(profile.gender) === 'גבר' 
-              ? 'man' 
+            SingleCharToString(profile.gender) === 'גבר'
+              ? 'man'
               : SingleCharToString(profile.gender) === 'אישה'
-                ? 'woman'
-                : 'male-female-outline'
+              ? 'woman'
+              : 'male-female-outline'
           }
-         ></TextView>
-        <TextView
-          title={'קצת עלי'}
-          content={
-            profile.introduction}
-        ></TextView>
-        <TagsView title={'תחומי עניין בטיול'} list={profile.tripInterests}></TagsView>
-        <TagsView title={'יעדים לטיול'} list={profile.travelPlan}></TagsView>
+        />
+        <TextView title={'קצת עלי'} content={profile.introduction} />
+        <TagsView
+          title={'תחומי עניין בטיול'}
+          list={profile.tripInterests}
+        />
+        <TagsView title={'יעדים לטיול'} list={profile.travelPlan} />
         <TextView
           iconName={'logo-instagram'}
           title={'אנסטגרם'}
           content={profile.instagram}
           allowCopy={true}
-        ></TextView>
+        />
+        <DropDown
+          header={'הטיולים שלי'}
+          content={
+            <FlatList
+              horizontal={true}
+              showsHorizontalScrollIndicator={false}
+              data={tripsRenderData}
+              renderItem={({ item }) => (
+                <SingleTrip
+                  handlePress={() => {
+                    navigation.navigate('ViewTrip', { trip: item })
+                  }}
+                  picUrl={{
+                    uri:
+                      item.tripPictureUrl ||
+                      'https://example.com/default-trip.png',
+                  }}
+                  title={item.tripName || 'Unnamed Trip'}
+                  destination={item.destinations || []}
+                  numOfPeople={item.joinedUsers.length || 0}
+                />
+              )}
+              onEndReachedThreshold={0.5}
+              onEndReached={() => {
+                if (isLoadingTrips) return
+                setIsLoadingTrips(true)
+                const contentToAppend = pagination(
+                  tripsRenderData,
+                  tripsCurrentPage + 1,
+                  tripsPageSize,
+                )
+                if (contentToAppend.length > 0) {
+                  setTripsCurrentPage(tripsCurrentPage + 1)
+                  setTripsRenderData((prev) => [...prev, ...contentToAppend])
+                }
+                setIsLoadingTrips(false)
+              }}
+            />
+          }
+        />
       </View>
     </ScrollView>
   )
@@ -94,11 +165,10 @@ export default function ViewProfile({navigation}) {
 
 const styles = StyleSheet.create({
   screen: {
-    flexGrow: '1',
+    flexGrow: 1,
     width: '100%',
     paddingVertical: 20,
     alignItems: 'center',
-    
   },
   title: {
     marginTop: windowHeight * 0.1,
@@ -108,7 +178,6 @@ const styles = StyleSheet.create({
     color: Theme.primaryColor.color,
   },
   inputsContainer: {
-    // marginTop: VerticalScale(44),
     display: 'flex',
     alignItems: 'flex-start',
     justifyContent: 'flex-start',
@@ -121,7 +190,7 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 5,
     justifyContent: 'center',
-    alignItems: 'flex-start', // Center the content horizontally
+    alignItems: 'flex-start',
     borderWidth: 1,
     borderColor: 'gray',
     marginBottom: 10,
@@ -138,7 +207,6 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     backgroundColor: Theme.primaryColor.color,
     borderRadius: 10,
-
   },
   avatarContainer: {
     marginTop: windowHeight * 0.1,
